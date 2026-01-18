e9c82e6603035e04ed4997d9bfbe4abd9b50e5da
KaiGeffen <geffenkai@gmail.com>
2026-01-18 02:27:07 -0500

    Fixed Adonis and Jules stories

--- client/src/data/avatarStories/Adonis9.ts
+++ client/src/data/avatarStories/Adonis9.ts
@@ -1,2 +1,1 @@
-export default `Coming soon...
-`
+export default `Coming soon...`
--- client/src/data/avatarStories/Jules9.ts
+++ client/src/data/avatarStories/Jules9.ts
@@ -1,2 +1,6 @@
-export default `Coming soon...
+export default `    That is where my story ends. Isn’t it? I cannot recall what it is I have yet to resolve. I’ve long since discarded the muddled, confused versions of myself. I’ve detached from pain, from anger, from inauthenticity, from any physical place or form. I’m ready to do the same to the mortal world and my own story. By now, you must know my story better than any other. Show me, what is left? What do I need to return to?
+
+OR
+
+    I see it now. I discarded too much, too quickly. I was eager to move on and take flight, I always have been. It’s hard to admit that every part of me never left, I still carry them with me. But saying that now feels oddly comforting, like I’ve given each hurt version of myself a kiss on the forehead before saying goodbye. That’s all I really needed. I think that in my next life I’ll let myself dance. 
 `
