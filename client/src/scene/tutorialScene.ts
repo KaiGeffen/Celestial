a67b05558741923ed1ada9f9531478c5e63154bd
KaiGeffen <geffenkai@gmail.com>
2026-01-22 02:47:50 -0500

    Fixed tutorial scene

--- client/src/scene/tutorialScene.ts
+++ client/src/scene/tutorialScene.ts
@@ -1,7 +1,7 @@
 import 'phaser'
 import RexUIPlugin from 'phaser3-rex-plugins/templates/ui/ui-plugin.js'
 
-import { JourneyMatchScene } from './matchScene'
+import { MatchScene } from './matchScene'
 import data from '../data/tutorial.json'
 import {
   Space,
@@ -22,7 +22,7 @@ import { Animation } from '../../../shared/animation'
 import { Zone } from '../../../shared/state/zone'
 import GameModel from '../../../shared/state/gameModel'
 
-export default class TutorialMatchScene extends JourneyMatchScene {
+export default class TutorialMatchScene extends MatchScene {
   // How far into the tutorial (How many lines of text you have seen)
   progress: number
 
