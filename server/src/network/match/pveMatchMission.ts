a1c0bb1793bf71af092fe699118dd825bc5841dc
KaiGeffen <geffenkai@gmail.com>
2026-03-15 19:50:43 -0400

    Journey missions have different avatars and titles

--- server/src/network/match/pveMatchMission.ts
+++ server/src/network/match/pveMatchMission.ts
@@ -6,25 +6,44 @@ import {
   getMissionById,
   missionToAiDeck,
 } from '../../../../shared/journey/journey'
+import { ServerController } from '../../gameController'
+import Catalog from '../../../../shared/state/catalog'
 
 class PveMatchMission extends PveMatch {
   private missionID: number
   private missionCards: number[]
 
-  constructor(
-    ws: ServerWS,
-    uuid: string,
-    deck: Deck,
-    missionID: number,
-  ) {
+  constructor(ws: ServerWS, uuid: string, deck: Deck, missionID: number) {
     const mission = getMissionById(missionID)
     if (!mission) {
       throw new Error(`Mission not found: ${missionID}`)
     }
+
+    // Get opponent's deck
     const aiDeck = missionToAiDeck(mission)
+
     super(ws, uuid, deck, aiDeck)
+
+    // Set important mission details for use later
     this.missionID = missionID
     this.missionCards = mission.cards ?? []
+  }
+
+  async startMatch() {
+    const user1 = await this.getUsernameElo(this.uuid1)
+
+    // Make a new game
+    this.game = new ServerController()
+    this.game.startGame(
+      this.deck1.cards.map((cardId) => Catalog.getCardById(cardId)),
+      this.deck2.cards.map((cardId) => Catalog.getCardById(cardId)),
+      this.deck1.cosmeticSet,
+      this.deck2.cosmeticSet,
+      user1.username,
+      'Umbra',
+      'My Friend',
+      'Curious Spirit',
+    )
   }
 
   protected async updateDatabases() {
