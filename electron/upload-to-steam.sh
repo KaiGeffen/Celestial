22e9ceacd20f9a152bc6977cdafdfc3d77c26162
KaiGeffen <geffenkai@gmail.com>
2026-04-24 13:25:58 -0400

    Fixed script, updated version

--- electron/upload-to-steam.sh
+++ electron/upload-to-steam.sh
@@ -11,13 +11,7 @@ cp -r "$DIST_DIR/mac/" "$CONTENT_DIR/mac/"
 cp -r "$DIST_DIR/win-unpacked/" "$CONTENT_DIR/win/"
 cp -r "$DIST_DIR/linux-unpacked/" "$CONTENT_DIR/linux/"
 
-echo "Uploading Mac build..."
-"$STEAMCMD" +login kivaent +run_app_build "$SCRIPTS_DIR/app_build_3810590_mac.vdf" +quit
-
-echo "Uploading Windows build..."
-"$STEAMCMD" +login kivaent +run_app_build "$SCRIPTS_DIR/app_build_3810590_win.vdf" +quit
-
-echo "Uploading Linux build..."
-"$STEAMCMD" +login kivaent +run_app_build "$SCRIPTS_DIR/app_build_3810590_linux.vdf" +quit
+echo "Uploading all platforms..."
+"$STEAMCMD" +login kivaent +run_app_build "$SCRIPTS_DIR/app_build_3810590.vdf" +quit
 
 echo "All platforms uploaded."
