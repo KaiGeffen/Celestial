5b9015da61ae135bb3c4340ed6f9dafe25440a2a
KaiGeffen <geffenkai@gmail.com>
2026-06-29 18:56:11 -0400

    Discord notification on match found after a discord message was sent

--- server/src/network/websocketServer.ts
+++ server/src/network/websocketServer.ts
@@ -49,6 +49,7 @@ class ActiveGame {
 interface WaitingPlayer {
   ws: ServerWS
   id: string
+  username: string
   deck: Deck
   activeGame: ActiveGame
   // Time when they queued
@@ -788,6 +789,20 @@ export default function createWebSocketServer() {
               // TODO Maybe just delete the last one? Somehow don't lose to race conditions
               delete searchingPlayers[data.password]
 
+              // If Discord was notified that this player was searching, follow up that a match was found
+              if (otherPlayer.notifiedDiscord) {
+                const webhookUrl = process.env.DISCORD_WEBHOOK_URL
+                if (webhookUrl) {
+                  fetch(webhookUrl, {
+                    method: 'POST',
+                    headers: { 'Content-Type': 'application/json' },
+                    body: JSON.stringify({
+                      content: `**${otherPlayer.username}** has found a match!`,
+                    }),
+                  }).catch((e) => console.error('Discord webhook error:', e))
+                }
+              }
+
               // Notify both players that they are connected
               // Start the match and let both users know the starting state
               await activeGame.match.startMatch()
@@ -797,6 +812,7 @@ export default function createWebSocketServer() {
               const waitingPlayer = {
                 ws: ws,
                 id: id,
+                username: username,
                 deck: data.deck,
                 activeGame: activeGame,
                 queuedAt: Date.now(),
@@ -1078,7 +1094,7 @@ export default function createWebSocketServer() {
               method: 'POST',
               headers: { 'Content-Type': 'application/json' },
               body: JSON.stringify({
-                content: `${username} is searching for an opponent.\nAre any <@&${DISCORD_ACTIVE_PLAYER_ROLE_ID}> around to help them out?`,
+                content: `**${username}** is searching for an opponent.\nAre any <@&${DISCORD_ACTIVE_PLAYER_ROLE_ID}> around to help them out?`,
                 allowed_mentions: {
                   roles: [DISCORD_ACTIVE_PLAYER_ROLE_ID],
                 },
