de68e4e66df34c9c2e9f4b1a09a8b61c3d8cbc93
KaiGeffen <geffenkai@gmail.com>
2026-07-25 18:29:47 -0400

    Updated the home page

--- client/src/data/announcements.ts
+++ client/src/data/announcements.ts
@@ -9,10 +9,16 @@ const ANNOUNCEMENT_PAIRS: { subheader: string; body: string }[] = [
   // },
   {
     subheader: 'Tournament',
-    body: `Our next tournament will kick off July 25th at 1 PM EST.
-[area=_link_tournament][stroke=${Color.goldS}]Register here![/stroke][/area]
+    body: `Our 13th tournament has concluded!
 
-250$ prize pool split as follows: $100 for 1st, $75 for 2nd, $50 for 3rd, and $25 consolation prize for a random non-placing participant. All attendees will receive an all-new cardback.`,
+Big thank you to the tournament organizers at Fate Leageu, and everyone who played (Check your inventory for the new cardback!)
+
+Huge congrats to the top 4 players:
+Rain, SubNova, ElineX, and Sespiny!`,
+    //     body: `Our next tournament will kick off July 25th at 1 PM EST.
+    // [area=_link_tournament][stroke=${Color.goldS}]Register here![/stroke][/area]
+
+    // 250$ prize pool split as follows: $100 for 1st, $75 for 2nd, $50 for 3rd, and $25 consolation prize for a random non-placing participant. All attendees will receive an all-new cardback.`,
   },
   // {
   //   subheader: 'Currencies & Cosmetics',
@@ -22,16 +28,20 @@ const ANNOUNCEMENT_PAIRS: { subheader: string; body: string }[] = [
   // },
   {
     subheader: 'Ranked',
-    body: `July 1st - 31st marks our second ranked season!
+    body: `Our second ranked season is nearly finished!
 
-Each player's ELO has been reset, and the #1 player at the end of the season picks the theme for the next cardback. Once it's ready, each player in the top 10 will get a free copy.`,
+Secure your spot in the top 10 by midnight EST on July 31st for the next cardback, and for the #1 player, the chance to help design that cardback!`,
   },
   {
-    subheader: 'New Cards',
-    body: `[area=_Liquidity][stroke=${Color.goldS}]Liquidity[/stroke][/area]
-[area=_Doll][stroke=${Color.goldS}]Doll[/stroke][/area]
-[area=_Heart][stroke=${Color.goldS}]Heart[/stroke][/area]
-[area=_Isolation][stroke=${Color.goldS}]Isolation[/stroke][/area]`,
+    subheader: '???',
+    body: `
+Something.... approaches...?
+
+I hear... [area=_Voices][stroke=${Color.goldS}]Voices[/stroke][/area]...
+
+Outside the walls... Howling...
+
+[area=_Abandoned][stroke=${Color.goldS}]Abandoned[/stroke][/area]...`,
 
     // [area=_Paramountcy][stroke=${Color.goldS}]Paramountcy[/stroke][/area] cards added 4 → 3
     // [area=_Heron][stroke=${Color.goldS}]Heron[/stroke][/area] cost 1 → 2
